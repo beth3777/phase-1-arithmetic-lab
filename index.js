f13273116e894276477e094ab372029229e85c5a
email <bethkimaku@gmail.com>
2023-03-26 22:57:05 +0300

    commit assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1=31
+let num2=2
+const multiply=(num1*num2);
 
-// Write your code here
+const random =Math.floor(Math.random())*10+1;
+console.log (random)
+
+let num3=10
+let num4=6
+const mod=(num3%num4)
+
+const max=Math.max(11,2,5,15,20);
+console.log(max)
